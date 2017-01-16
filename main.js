db1566275d031ad42f33f75db698e76b23abc6dd
mmarla <susanar000@gmail.com>
2017-01-16 14:53:07 +0000

    Add basic express server

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 
+app.get('/', function (req, res) {
+  res.status(200).json({status: 'OK'});
+});
+
+app.get('*', function (req, res) {
+  res.redirect('/');
+});
+
 app.listen(process.env.PORT || 3000, function (error, result) {
   if (error) console.log(error);
   console.log(`Listening on port 3000`);
