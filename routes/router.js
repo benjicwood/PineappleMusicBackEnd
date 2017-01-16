09a64fa4d982a64095de8194b15149fba8bb3856
mmarla <susanar000@gmail.com>
2017-01-16 21:18:18 +0000

    Add api/match/musician/:id and test

--- routes/router.js
+++ routes/router.js
@@ -26,4 +26,9 @@ router.get('/profile/musician/:id', function (req, res) {
   );
 });
 
+router.get('/match/musician/:id', function (req, res) {
+  res.status(200).json({status: '/match/musician/:id OK'}
+  );
+});
+
 module.exports = router;
