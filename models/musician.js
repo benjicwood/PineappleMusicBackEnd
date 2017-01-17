02e10e4a58bfcd5faf81e10a3382b7135227dce0
mmarla <susanar000@gmail.com>
2017-01-17 15:53:18 +0000

    Add mongoose models

---
+++ models/musician.js
@@ -0,0 +1,50 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let MusicianSchema = new Schema({
+  type: {
+    type: String,
+    required: false
+  },
+  email: {
+    type: String,
+    required: false
+  },
+  phone_number: {
+    type: Number,
+    required: false
+  },
+  username: {
+    type: String,
+    required: false
+  },
+  profile_pic: {
+    type: String,
+    required: false
+  },
+  join_date: {
+    type: Date,
+    required: false
+  },
+  location: {
+    type: Object,
+    required: false
+  },
+  instrument: {
+    type: String,
+    required: false
+  },
+  genre: {
+    type: String,
+    required: false
+  },
+  last_seen: {
+    type: String,
+    required: false
+  }
+},
+  {
+    collection: 'musician'
+  });
+
+module.exports = mongoose.model('musician', MusicianSchema);
