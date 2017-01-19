900bca696c04fdee9ce42385c6618f60f5fc8bde
mmarla <susanar000@gmail.com>
2017-01-19 15:43:07 +0000

    Link controllers to routes

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,9 @@
 const router = require('express').Router();
 
+const retrieve = require('../controllers/controller.js').retrieve;
+const create = require('../controllers/controller.js').create;
+const update = require('../controllers/controller.js').update;
+
 router.get('/', function (req, res) {
   res.send({
     version: '1.0.0'
@@ -7,73 +11,96 @@ router.get('/', function (req, res) {
 });
 
 router.get('/genre', function (req, res) {
-  res.status(200).json({status: '/genre OK'}
-  );
+  retrieve.retrieveGenres(req, function (error, data) {
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
 });
 
 router.get('/instrument', function (req, res) {
-  res.status(200).json({status: '/instrument OK'}
-  );
+  retrieve.retrieveInstruments(req, function (error, data) {
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
 });
 
 router.get('/profile/band/:id', function (req, res) {
-  res.status(200).json({status: '/profile/band/:id OK'}
-  );
+  retrieve.retrieveBandProfile(req.params.id, function (error, data) {
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
 });
 
 router.get('/profile/musician/:id', function (req, res) {
-  res.status(200).json({status: '/profile/musician/:id OK'}
-  );
+  retrieve.retrieveMusicianProfile(req.params.id, function (error, data) {
+    if (error) res.status(500).send(error);
+    console.log('.get musician profile query success. Sending data.');
+    res.send(data);
+  });
 });
 
 router.get('/ilike/:id', function (req, res) {
-  res.status(200).json({status: '/ilike/:id OK'}
-  );
+  retrieve.retrieveILike(req.params.id, function (error, data) {
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
 });
 
 router.get('/likesme/:id', function (req, res) {
-  res.status(200).json({status: '/likesme/:id OK'}
-  );
+  retrieve.retrieveLikesMe(req.params.id, function (error, data) {
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
 });
 
-router.get('/match/musician/:id', function (req, res) {
-  res.status(200).json({status: '/match/musician/:id OK'}
-  );
-});
-
-router.get('/match/band/:id', function (req, res) {
-  res.status(200).json({status: '/match/band/:id OK'}
-  );
+router.get('/matches', function (req, res) {
+  retrieve.retrieveMatches(req.body, function (error, data) {
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
 });
 
 router.post('/profile/band', function (req, res) {
-  res.status(200).json({status: '/profile/band OK'}
-  );
+  create.createBandProfile(req.body, function (error, data) {
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
 });
 
 router.post('/profile/musician', function (req, res) {
-  res.status(200).json({status: 'POST /profile/musician OK'}
-  );
-});
-
-router.post('/profile/band/:id', function (req, res) {
-  res.status(200).json({status: 'POST /profile/band/:id OK'}
-  );
-});
-
-router.post('/profile/musician/:id', function (req, res) {
-  res.status(200).json({status: 'POST /profile/musician/:id OK'}
-  );
+  create.createMusicianProfile(req.body, function (error, data) {
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
 });
 
 router.post('/connections', function (req, res) {
-  res.status(200).json({status: 'POST /connections OK'}
-  );
+  create.createConnection(req.body, function (error, data) {
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
 });
 
 router.post('/donotdisplay', function (req, res) {
-  res.status(200).json({status: 'POST /donotdisplay OK'}
-  );
+  create.createDoNotDisplay(req.body, function (error, data) {
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
+});
+
+// update routes
+router.post('/profile/band/:id', function (req, res) {
+  update.updateBandProfile(req.body, function (error, data) {
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
+});
+
+router.post('/profile/musician/:id', function (req, res) {
+  update.updateMusicianProfile(req.body, function (error, data) {
+    if (error) res.status(500).send(error);
+    res.send(data);
+  });
 });
 
 module.exports = router;
