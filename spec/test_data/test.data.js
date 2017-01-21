e2afe61f55c0ba98cd8ee9fa2dadcf0aabac5b97
mmarla <susanar000@gmail.com>
2017-01-21 17:50:56 +0000

    Refactor seed data

--- spec/test_data/test.data.js
+++ spec/test_data/test.data.js
@@ -6,13 +6,11 @@ let band = JSON.parse(fs.readFileSync(path.join(__dirname, './test.band.json'), 
 let genre = JSON.parse(fs.readFileSync(path.join(__dirname, './test.genre.json'), 'utf8'));
 let instrument = JSON.parse(fs.readFileSync(path.join(__dirname, './test.instrument.json'), 'utf8'));
 let connection = JSON.parse(fs.readFileSync(path.join(__dirname, './test.connection.json'), 'utf8'));
-let donotdisplay = JSON.parse(fs.readFileSync(path.join(__dirname, './test.donotdisplay.json'), 'utf8'));
 
 module.exports = {
   testMusician: musician,
   testBand: band,
   testInstrument: instrument,
   testGenre: genre,
-  testConnection: connection,
-  testDonotdisplay: donotdisplay
+  testConnection: connection
 };
