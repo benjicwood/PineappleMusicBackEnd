434ee3918ded72ae70ec57440bd68c0badb89675
mmarla <susanar000@gmail.com>
2017-01-26 19:13:40 +0000

    Update update.js

--- controllers/update.js
+++ controllers/update.js
@@ -1,14 +1,14 @@
 const models = require('../models/models.js');
 
 const musicianProfile = function (query, id, callback) {
-  models.Musician.update({'_id': id}, query, function (error, docs) {
+  mmodels.Musician.findOneAndUpdate({'_id': id}, query, {new: true}, function (error, docs) {
     if (error) return callback(error);
     callback(null, docs);
   });
 };
 
 const bandProfile = function (query, id, callback) {
-  models.Band.update({'_id': id}, query, function (error, docs) {
+  models.Band.findOneAndUpdate({'_id': id}, query, {new: true}, function (error, docs) {
     if (error) return callback(error);
     callback(null, docs);
   });
